780bffb608e6cc88e506e6d99dcf87c41f6a52f5
anurag-rudra <anurag.rudra@gmail.com>
2026-03-21 19:36:47 +1100

    added value

--- infra/modules/gateway_validation/main.tf
+++ infra/modules/gateway_validation/main.tf
@@ -29,10 +29,10 @@ variable "python_script_path" {
   default     = "../tools/validate_running_gateway.py"
 }
 
-variable "yaml_config_path" {
-  description = "Path to the YAML configuration file"
-  type        = string
-}
+# variable "yaml_config_path" {
+#   description = "Path to the YAML configuration file"
+#   type        = string
+# }
 
 variable "ingestion_pipeline_ids" {
   description = "List of ingestion pipeline IDs to validate after gateway is running"
