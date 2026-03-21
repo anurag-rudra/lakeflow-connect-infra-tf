6a7a196de94c85eb7e98cc23e945c977ce9f2cd4
anurag-rudra <anurag.rudra@gmail.com>
2026-03-21 19:32:55 +1100

    added value

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,5 @@
 variable "yaml_config_path" {
   description = "Relative or absolute path to YAML file where all the configurations are placed"
   type        = string
-  default     = "../config/lakeflow_dev.yml"
+  default     = "${path.module}/config/lakeflow_dev.yml"
 }
